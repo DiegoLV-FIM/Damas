0eb6ae2b9bc21a9f40f2802dc21f9be0ac964522
Diego Leyva <20024568@ms.uas.edu.mx>
2026-09-21 01:30:35 -0700

    Seleccion de Casilla

--- Damas.cpp
+++ Damas.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <cstdlib>
 
 constexpr Color BACKGROUND = BEIGE;
 
@@ -13,6 +14,17 @@ class Damas {
     QLight,
     Dark,
     QDark,
+  };
+
+  class Pos {
+  public:
+    int x, y;
+
+    Pos(int _x, int _y) : x{_x}, y{_y} {};
+    Pos(Vector2 &vec) : x{(int)vec.x}, y{(int)vec.y} {}
+    Pos(Vector2 &&vec) : x{(int)vec.x}, y{(int)vec.y} {}
+
+    bool operator==(Pos &other) { return x == other.x && y == other.y; }
   };
 
   Tile tablero[8][8];
@@ -32,14 +44,27 @@ public:
     }
   }
 
+  static inline Vector2 getTilePos(Pos tile, Vector2 pos, float size) noexcept {
+    return pos + Vector2{(float)tile.x, (float)tile.y} * size;
+  }
+
   void Draw(Vector2 pos, float size) noexcept {
     DrawRectangle(pos.x, pos.y, size, size, LIGHT_TILE);
     float step = size / 8.f;
+
+    {
+      Vector2 mouse = (GetMousePosition() - pos) / step;
+      if (Vector2Clamp(mouse, Vector2Zero(), {7.f,7.f}) == mouse) {
+        Vector2 mouse_tile = getTilePos(mouse, pos, step);
+        DrawRectangle(mouse_tile.x, mouse_tile.y, step, step,
+                      ColorBrightness(LIGHT_TILE, -0.3f));
+      }
+    }
+
     for (int y = 0; y < 8; y++) {
       for (int x = y % 2; x < 8; x += 2) {
-        Vector2 rpos =
-            Vector2Add(pos, Vector2Scale({(float)x, (float)y}, step));
-        DrawRectangle(rpos.x, rpos.y, step, step, DARK_TILE);
+        Vector2 tile_pos = getTilePos({x, y}, pos, step);
+        DrawRectangle(tile_pos.x, tile_pos.y, step, step, DARK_TILE);
       }
     }
 
@@ -47,10 +72,9 @@ public:
 
     for (int y = 0; y < 8; y++) {
       for (int x = 1 - (y % 2); x < 8; x += 2) {
-        Vector2 rpos =
-            Vector2Add(pos, Vector2Scale({(float)x, (float)y}, step));
-
-        DrawTile(tablero[y][x], Vector2AddValue(rpos, step * .5f), step * .32f);
+        Vector2 tile_pos = getTilePos({x, y}, pos, step);
+        DrawTile(tablero[y][x], Vector2AddValue(tile_pos, step * .5f),
+                 step * .32f);
       }
     }
   }
@@ -69,7 +93,8 @@ public:
     default:
       break;
     }
-    DrawCircleV(center, radius, color);
+    DrawCircleV(center, radius, BLACK);
+    DrawCircleV(center, radius - 4.f, color);
   }
 
   static void DrawBoardLines(Vector2 pos, float size) noexcept {
@@ -89,7 +114,7 @@ public:
 int main() {
   Damas damas;
 
-  InitWindow(800, 700, "Hola Mundo");
+  InitWindow(800, 700, "Damas");
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
@@ -97,7 +122,6 @@ int main() {
     ClearBackground(BACKGROUND);
     damas.Draw(Vector2{100, 50}, 600);
     EndDrawing();
-    // break;
   }
   CloseWindow();
 
