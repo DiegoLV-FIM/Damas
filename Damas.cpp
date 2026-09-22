e9091fffba13a87684b575f6d764d3a63516fcff
Diego Leyva <20024568@ms.uas.edu.mx>
2026-09-21 21:50:14 -0700

    Limpieza de codigo

--- Damas.cpp
+++ Damas.cpp
@@ -1,6 +1,5 @@
 #include "raylib.h"
 #include "raymath.h"
-#include <cstdlib>
 
 constexpr Color BACKGROUND = BEIGE;
 
@@ -21,13 +20,14 @@ class Damas {
     int x, y;
 
     Pos(int _x, int _y) : x{_x}, y{_y} {};
-    Pos(Vector2 &vec) : x{(int)vec.x}, y{(int)vec.y} {}
-    Pos(Vector2 &&vec) : x{(int)vec.x}, y{(int)vec.y} {}
+    Pos(const Vector2 &vec) : x{(int)vec.x}, y{(int)vec.y} {}
+    Pos(const Vector2 &&vec) : x{(int)vec.x}, y{(int)vec.y} {}
 
     bool operator==(Pos &other) { return x == other.x && y == other.y; }
   };
 
   Tile tablero[8][8];
+  bool turno_blanco = true;
 
 public:
   Damas() noexcept {
@@ -48,33 +48,20 @@ public:
     return pos + Vector2{(float)tile.x, (float)tile.y} * size;
   }
 
-  void Draw(Vector2 pos, float size) noexcept {
-    DrawRectangle(pos.x, pos.y, size, size, LIGHT_TILE);
-    float step = size / 8.f;
-
-    {
-      Vector2 mouse = (GetMousePosition() - pos) / step;
-      if (Vector2Clamp(mouse, Vector2Zero(), {7.f,7.f}) == mouse) {
-        Vector2 mouse_tile = getTilePos(mouse, pos, step);
-        DrawRectangle(mouse_tile.x, mouse_tile.y, step, step,
-                      ColorBrightness(LIGHT_TILE, -0.3f));
-      }
+  static inline void HighlightMouse(Vector2 pos, float step) noexcept {
+    const Vector2 mouse = (GetMousePosition() - pos) / step;
+    if (Vector2Clamp(mouse, Vector2Zero(), {7.f, 7.f}) == mouse) {
+      const Vector2 mouse_tile = getTilePos(mouse, pos, step);
+      DrawRectangle(mouse_tile.x, mouse_tile.y, step, step,
+                    ColorBrightness(LIGHT_TILE, -0.3f));
     }
+  }
 
+  static inline void DrawDarkTiles(Vector2 pos, float step) noexcept {
     for (int y = 0; y < 8; y++) {
       for (int x = y % 2; x < 8; x += 2) {
-        Vector2 tile_pos = getTilePos({x, y}, pos, step);
+        const Vector2 tile_pos = getTilePos({x, y}, pos, step);
         DrawRectangle(tile_pos.x, tile_pos.y, step, step, DARK_TILE);
-      }
-    }
-
-    DrawBoardLines(pos, size);
-
-    for (int y = 0; y < 8; y++) {
-      for (int x = 1 - (y % 2); x < 8; x += 2) {
-        Vector2 tile_pos = getTilePos({x, y}, pos, step);
-        DrawTile(tablero[y][x], Vector2AddValue(tile_pos, step * .5f),
-                 step * .32f);
       }
     }
   }
@@ -97,14 +84,34 @@ public:
     DrawCircleV(center, radius - 4.f, color);
   }
 
+  void DrawTiles(Vector2 pos, float step) noexcept {
+    for (int y = 0; y < 8; y++) {
+      for (int x = 1 - (y % 2); x < 8; x += 2) {
+        const Vector2 tile_pos = getTilePos({x, y}, pos, step);
+        DrawTile(tablero[y][x], Vector2AddValue(tile_pos, step * .5f),
+                 step * .32f);
+      }
+    }
+  }
+
+  void Draw(Vector2 pos, float size) noexcept {
+    DrawRectangle(pos.x, pos.y, size, size, LIGHT_TILE);
+    const float step = size / 8.f;
+
+    HighlightMouse(pos, step);
+    DrawDarkTiles(pos, step);
+    DrawBoardLines(pos, size);
+    DrawTiles(pos, step);
+  }
+
   static void DrawBoardLines(Vector2 pos, float size) noexcept {
     constexpr float thick = 4.f;
     constexpr float half_thick = thick / 2;
-    float step = size / 8.f;
+    const float step = size / 8.f;
 
-    Vector2 end = Vector2AddValue(pos, half_thick + size);
+    const Vector2 end = Vector2AddValue(pos, half_thick + size);
     for (int i = 0; i <= 8; i++) {
-      Vector2 off = Vector2AddValue(pos, step * i);
+      const Vector2 off = Vector2AddValue(pos, step * i);
       DrawLineEx({off.x, pos.y}, {off.x, end.y}, thick, BLACK);
       DrawLineEx({pos.x, off.y}, {end.x, off.y}, thick, BLACK);
     };
